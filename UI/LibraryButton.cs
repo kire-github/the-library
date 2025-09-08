8cf6c7bd7f5f66fc56e6b1b5917b9dff5e07aa75
kire <ekeducatio@gmail.com>
2025-09-08 15:40:36 +0200

    Add button to the game's pause menu

--- UI/LibraryButton.cs
+++ UI/LibraryButton.cs
@@ -5,9 +5,9 @@ namespace TheLibrary.UI
 {
     public class LibraryButton
     {
-        public static void Build()
+        public static void BuildMainMenuButton()
         {
-            if (GameObject.Find("modded_Button_Library") != null)
+            if (GameObject.Find("modded_Button_MainLibrary") != null)
                 return;
 
             GameObject originalButton = GameObject.Find("Button_Credits");
@@ -18,10 +18,34 @@ namespace TheLibrary.UI
             if (libraryButtonObj == null)
                 return;
 
-            libraryButtonObj.name = "modded_Button_Library";
+            libraryButtonObj.name = "modded_Button_MainLibrary";
             libraryButtonObj.SetActive(true);
 
             UIUtils.ReplaceText(libraryButtonObj.transform, "Text", "The Library");
+            UIUtils.ReplaceSprite(libraryButtonObj.transform, "TokenArt", "images/aspects/secrethistories");
+            UIUtils.ReplaceButtonOnClick(libraryButtonObj.GetComponent<Button>().transform, () =>
+            {
+                LibraryMenu.Toggle();
+            });
+        }
+
+        public static void BuildPauseMenuButton()
+        {
+            if (GameObject.Find("modded_Button_PauseLibrary") != null)
+                return;
+
+            GameObject originalButton = GameObject.Find("ButtonResume");
+            if (originalButton == null)
+                return;
+
+            GameObject libraryButtonObj = Object.Instantiate(originalButton.gameObject, originalButton.transform.parent);
+            if (libraryButtonObj == null)
+                return;
+
+            libraryButtonObj.name = "modded_Button_TheLibrary";
+            libraryButtonObj.SetActive(true);
+
+            UIUtils.ReplaceText(libraryButtonObj.transform, "Label", "The Library");
             UIUtils.ReplaceSprite(libraryButtonObj.transform, "TokenArt", "images/aspects/secrethistories");
             UIUtils.ReplaceButtonOnClick(libraryButtonObj.GetComponent<Button>().transform, () =>
             {
